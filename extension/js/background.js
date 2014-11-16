860f93cee7a6d1fea7459ae20982570c4a14f033
Rizwan Sharif <rizwan.sharif@audaxhealth.com>
2014-11-15 17:38:45 -0800

    fix multiple events problem with surfy

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,37 +1,25 @@
+var surfyUrl = "http://localhost:3000";
 function showExtension(tabId, changeInfo, tab) {
 
-
-    var Url = "http://localhost:3000";
-
-    if(changeInfo.status == 'complete') {
-        console.log(changeInfo);
-
-        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-
-            var currentUrl = encodeURIComponent(tabs[0].url);
-
-            if (tabs[0].url !== "chrome://newtab/") {
-
-                $.get(Url + "/rating/" + currentUrl).done(function (data) {
-                    chrome.pageAction.show(tabId);
-                    console.log(data);
-                    if (data.rating < 3) {
-                        chrome.pageAction.setIcon({tabId: tabId, path: "images/low_star.png"});
-                    } else if (data.rating === 3) {
-                        chrome.pageAction.setIcon({tabId: tabId, path: "images/icon.png"});
-                    } else {
-                        chrome.pageAction.setIcon({tabId: tabId, path: "images/goodstar.png"});
-                    }
-                });
-
-                chrome.pageAction.onClicked.addListener(function () {
-                    chrome.tabs.sendMessage(tabs[0].id, {loadComments: true}, function (response) {
-                        console.log("response from content script" + response);
-                    });
-                });
+    if (tab.status === "complete" && tab.url !== "chrome://newtab/") {
+        var currentUrl = encodeURIComponent(tab.url);
+        $.get(surfyUrl + "/rating/" + currentUrl).done(function (data) {
+            chrome.pageAction.show(tabId);
+            if (data.rating < 3) {
+                chrome.pageAction.setIcon({tabId: tabId, path: "images/low_star.png"});
+            } else if (data.rating === 3) {
+                chrome.pageAction.setIcon({tabId: tabId, path: "images/icon.png"});
+            } else {
+                chrome.pageAction.setIcon({tabId: tabId, path: "images/goodstar.png"});
             }
         });
     }
 };
 
 chrome.tabs.onUpdated.addListener(showExtension);
+
+chrome.pageAction.onClicked.addListener(function (tab) {
+    chrome.tabs.sendMessage(tab.id, {loadComments: true}, function (response) {
+        console.log("response from content script" + response);
+    });
+});
